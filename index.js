c833c61da2a9c4f41f19704cbb5d27d3cf12b305
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-11-02 23:32:18 +0530

    mysql2 package

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+
+const connnection = mysql.createConnection({
+  host : 'localhost',
+  user:'root',
+  database:'test'
+})
 
 let GetRandomUser = () => {
   return {
