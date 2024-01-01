0cda9a0db7741b4ee484c3d784c7118964b63e4c
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-01 13:48:46 +0530

    using sql in terminal running SHOW TABLES query using node

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
 
-const connnection = mysql.createConnection({
+const connection = mysql.createConnection({
   host : 'localhost',
   user:'root',
   database:'D_APP',
-  password : '1234'
+  password : '1234' // mysql@123
 });
 //sql injected
-connnection.query("SHOW TABLES", (err,res)=>{
+let query = "SHOW TABLES";
+connection.query(query, (err,res)=>{
   try {
     if (err) throw err;
     console.log(res);
+    console.log(res.length);
   } catch (error) {
     console.log(err);
   }
 });
+
+connection.end();
+
 
 let GetRandomUser = () => {
   return {
